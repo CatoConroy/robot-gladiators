1cf3f522e8a995b8803026501c83d1dc23c887d5
CatoConroy <catoconroy@protonmail.com>
2022-11-15 02:30:45 -0600

    initial commit/fix

---
+++ assets/js/game.js
@@ -0,0 +1,12 @@
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+
+let playerName = window.prompt ("What is your robot's name?")
+
+console.log(playerName);
+
+let name = "your name";
+console.log(name);
+// fight(); 
