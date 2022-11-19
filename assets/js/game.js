e032fc2f0d29638f42257dd3b585fac9215ad374
CatoConroy <catoconroy@protonmail.com>
2022-11-19 13:29:44 -0600

    fixed faulty push

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,3 @@
-function fight () {
-    window.alert("The fight has begun!");
-}
-
-
-let playerName = window.prompt ("What is your robot's name?")
-
-console.log(playerName);
-
-let name = "your name";
-console.log(name);
-// fight(); 
 let playerName = window.prompt("What is your robot's name?");
 let playerHealth = 100;
 let playerAttack = 10;
